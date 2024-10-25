df2a48475e8a4a9d884c89522776556d80cb5c33
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2024-10-25 12:15:16 +0530

    Update stream.py

--- ISTKHAR/utils/stream/stream.py
+++ ISTKHAR/utils/stream/stream.py
@@ -11,7 +11,7 @@ from ISTKHAR.misc import db
 from ISTKHAR.utils.database import add_active_video_chat, is_active_chat
 from ISTKHAR.utils.exceptions import AssistantErr
 from ISTKHAR.utils.inline import aq_markup, close_markup, stream_markup
-from ISTKHAR.utils.pastebin import PURVI Bin
+from ISTKHAR.utils.pastebin import PURVIBin
 from ISTKHAR.utils.stream.queue import put_queue, put_queue_index
 from ISTKHAR.utils.thumbnails import get_thumb
 
