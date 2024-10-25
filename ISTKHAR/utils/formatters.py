fcba877578237e12737121da1e3a185cdb470cfd
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2024-10-25 11:34:33 +0530

    Update formatters.py

--- ISTKHAR/utils/formatters.py
+++ ISTKHAR/utils/formatters.py
@@ -39,20 +39,20 @@ def convert_bytes(size: float) -> str:
     return "{:.2f} {}B".format(size, power_dict[t_n])
 
 
-async def int_to_thunder (user_id: int) -> str:
-    thunder bet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+async def int_to_alpha (user_id: int) -> str:
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
     text = ""
     user_id = str(user_id)
     for i in user_id:
-        text += thunder bet[int(i)]
+        text += alphabet[int(i)]
     return text
 
 
-async def thunder _to_int(user_id_thunder bet: str) -> int:
-    thunder bet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+async def  alpha_to_int(user_id_alphabet: str) -> int:
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
     user_id = ""
-    for i in user_id_thunder bet:
-        index = thunder bet.index(i)
+    for i in user_id_alphabet:
+        index = alphabet.index(i)
         user_id += str(index)
     user_id = int(user_id)
     return user_id
