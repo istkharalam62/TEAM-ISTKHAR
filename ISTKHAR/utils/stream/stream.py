d6ed563f9cf709fa7d40f973312ee022502758dc
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2024-10-25 12:18:53 +0530

    Update stream.py

--- ISTKHAR/utils/stream/stream.py
+++ ISTKHAR/utils/stream/stream.py
@@ -116,7 +116,7 @@ async def stream(
         if count == 0:
             return
         else:
-            link = await PURVI Bin(msg)
+            link = await PURVIBin(msg)
             lines = msg.count("\n")
             if lines >= 17:
                 car = os.linesep.join(msg.split(os.linesep)[:17])
